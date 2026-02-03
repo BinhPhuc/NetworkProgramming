9d9853e3684eae509ce37081f54122ac1edce988
Binh Phuc <pham.phuc.binh.271106@gmail.com>
2026-02-03 19:04:34 +0700

    follow this

--- server.c
+++ server.c
@@ -2,9 +2,11 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 int main() {
   struct addrinfo hints, *res;
@@ -16,29 +18,35 @@ int main() {
   hints.ai_flags = AI_PASSIVE; // For wildcard IP address
   if ((status = getaddrinfo(NULL, "8080", &hints, &res)) != 0) {
     fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-    return -1;
+    return EXIT_FAILURE;
   }
   struct addrinfo *rp;
+  int sockfd;
   for (rp = res; rp != NULL; rp = rp->ai_next) {
-    int sockfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+    sockfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
     if (sockfd == -1) {
       perror("socket");
       continue;
     }
     int yes = 1;
     setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
-    if (bind(sockfd, rp->ai_addr, rp->ai_addrlen) == -1) {
-      perror("bind");
-      continue;
+    if (bind(sockfd, rp->ai_addr, rp->ai_addrlen) == 0) {
+      break;
     }
-    if (listen(sockfd, 10) == -1) {
-      perror("listen");
-      continue;
-    }
-    printf("Server listening on port 8080\n");
-    // Accept a single connection for demonstration purposes
-    int new_fd = accept(sockfd, (struct sockaddr *)&their_addr,
-                        &(socklen_t){sizeof(their_addr)});
+    close(sockfd);
   }
+  freeaddrinfo(res);
+  if (rp == NULL) {
+    fprintf(stderr, "Could not bind\n");
+    return EXIT_FAILURE;
+  }
+  if (listen(sockfd, 10) == -1) {
+    perror("listen");
+    return EXIT_FAILURE;
+  }
+  printf("Server listening on port 8080\n");
+  int new_fd = accept(sockfd, (struct sockaddr *)&their_addr,
+                      &(socklen_t){sizeof(their_addr)});
+  // Do something with new_fd: send, rev data, create new thread, etc.
   return 0;
 }
