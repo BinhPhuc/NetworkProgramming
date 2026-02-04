db0b4ba40c3a54216f931a585a56286773312ed1
Binh Phuc <pham.phuc.binh.271106@gmail.com>
2026-02-04 23:44:50 +0700

    Adapt client to connect locally and send messages

--- client.c
+++ client.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 int main() {
   int status;
@@ -12,34 +13,33 @@ int main() {
   memset(&hints, 0, sizeof(hints));
   hints.ai_family = AF_UNSPEC; // Allow IPv4 or IPv6
   hints.ai_socktype = SOCK_STREAM;
-  if ((status = getaddrinfo("youtube.com", "80", &hints, &res) != 0)) {
+  if ((status = getaddrinfo("127.0.0.1", "8080", &hints, &res) != 0)) {
     fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
     return -1;
   }
   struct addrinfo *p;
   char ipaddr[INET6_ADDRSTRLEN];
+  int sockfd;
   for (p = res; p != NULL; p = p->ai_next) {
-    char *ipver;
-    void *addr;
-    if (p->ai_family == AF_INET) {
-      ipver = "IPv4";
-      struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
-      addr = &(ipv4->sin_addr);
-    } else {
-      ipver = "IPv6";
-      struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
-      addr = &(ipv6->sin6_addr);
-    }
-    inet_ntop(p->ai_family, addr, ipaddr, INET6_ADDRSTRLEN);
-    printf("%s: %s\n", ipver, ipaddr);
-    int sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+    sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
     if (sockfd == -1) {
       perror("socket");
       continue;
     }
-    int con = connect(sockfd, p->ai_addr, p->ai_addrlen);
+    if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+      close(sockfd);
+      perror("connect");
+      continue;
+    }
+    break; // connected successfully
   }
-  // printf("Finished resolving youtube.com\n");
   freeaddrinfo(res);
+  if (p == NULL) {
+    fprintf(stderr, "Could not connect\n");
+    return -1;
+  }
+  char *msg = "Hey server! How are you today!";
+  int bytes_sent = send(sockfd, msg, strlen(msg), 0);
+  printf("Sent %d bytes to server\n", bytes_sent);
   return 0;
 }
